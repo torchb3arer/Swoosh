6ff02fa438e9a57d4c5a1fa76c4964cd34cd2ed8
Torchb3arer <sergio.caceres.vera@gmail.com>
2018-08-16 19:38:30 -0300

    avances 6

--- app/src/main/java/com/torchbapps/swoosh/Constants.kt
+++
@@ -1,3 +0,0 @@
-package com.torchbapps.swoosh
-
-const val EXTRA_LEAGUE= "league"
---
+++ app/src/main/java/com/torchbapps/swoosh/Controller/FinishActivity.kt
@@ -0,0 +1,21 @@
+package com.torchbapps.swoosh.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.torchbapps.swoosh.R
+import com.torchbapps.swoosh.Utilities.EXTRA_LEAGUE
+import com.torchbapps.swoosh.Utilities.EXTRA_SKILL
+import kotlinx.android.synthetic.main.activity_finish.*
+
+class FinishActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finish)
+
+        val league = intent.getStringExtra(EXTRA_LEAGUE)
+        val skill = intent.getStringExtra(EXTRA_SKILL)
+
+        searchLeagueTxt.text = "Looking for $league $skill league near you ..."
+    }
+}
